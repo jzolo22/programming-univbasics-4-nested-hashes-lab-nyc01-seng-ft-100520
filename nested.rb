cd1eedb10fcbcb045729e4d157b1b1d3e3b7f60d
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-12 16:23:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,10 +87,14 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    :yukihiro_matsumoto => {
-  :known_for => "Ruby",
-  :languages => ["LISP", "C"]
+
+
 }
+programmer_hash[:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]]
+  
+  programmer_hash
 
 end
 
